a99b4d9518b626d0f0ea89a1e84446aa6603bcf1
dikaantoniputra <dikahanggara2@gmail.com>
2025-07-19 10:06:48 +0700

    first commit

---
+++ soal1/database/migrations/2025_07_18_163118_create_buses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buses', function (Blueprint $table) {
+            $table->id();
+            $table->string('nopol');
+            $table->string('tahun_pembuatan');
+            $table->string('tipe');
+            $table->string('merek');
+            $table->string('kapasitas');
+            $table->string('jenis_kendaraan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buses');
+    }
+};
